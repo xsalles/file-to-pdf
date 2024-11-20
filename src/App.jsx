3c9a86fdb7b0ce8c0c489ddef6e794d04c1d6113
xsalles <xs.salles@gmail.com>
2024-11-19 22:29:38 -0300

    crated reposiroty.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <main className="container">
+      <div className="input-container">
+        <h1>Anexe a imagem que você deseja transformar em PDF pelo upload</h1>
+        <input type="file" name="" id="" />
+      </div>
+    </main>
+  );
+}
